48d92d50c03086a47211cc67acc0b1be1e5d999b
MarkAbram7881 <markabram7881@gmail.com>
2026-06-04 14:41:10 +0100

    update

--- preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('storage', {
-    loadData: () => ipcRenderer.invoke('load-data'),
-    saveData: (data) => ipcRenderer.invoke('save-data', data)
-});
